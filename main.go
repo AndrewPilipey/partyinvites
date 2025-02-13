ba3f9a9e8d9fa1d486ac762b59d80be27be5e714
AndrewPilipey <andrewpilipeyone@gmail.com>
2025-02-13 15:11:43 +0300

    Some changes

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+)
 
 type Rsvp struct {
 	Name, Email, Phone string
@@ -8,7 +10,12 @@ type Rsvp struct {
 } // создание структуры - определение пользовательских типов данных
 
 var responses = make([]*Rsvp, 0, 10) //определение среза для ответов гостей (без фикс.длины, т.к. неизвестно сколько будет гостей)
+var templates = make(map[string]*template.Template, 3)
+
+func loadTemplates() {
+	// TODO - load templates here
+}
 
 func main() {
-	fmt.Println("TODO: add some features")
+	loadTemplates()
 }
