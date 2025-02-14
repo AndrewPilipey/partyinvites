31f9f8a1901584e6dd84fac635f9378a75e32559
AndrewPilipey <andrewpilipeyone@gmail.com>
2025-02-14 09:53:46 +0300

    continuation of work

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"net/http"
 )
 
 type Rsvp struct {
@@ -13,9 +15,33 @@ var responses = make([]*Rsvp, 0, 10) //определение среза для 
 var templates = make(map[string]*template.Template, 3)
 
 func loadTemplates() {
-	// TODO - load templates here
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for index, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
+
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, nil)
+}
+
+func listHadler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, responses)
 }
 
 func main() {
 	loadTemplates()
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/list", listHadler)
+
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
